2a8e39c17ac522532ac5e59ab0d6a7da1170e20a
itziarpb <itziar.perezb@gmail.com>
2022-11-19 00:05:10 +0000

    inicio  css e intento hover

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,48 +1,60 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const [inputValue, setInputValue] = useState ()
-	const [task, setTask] = useState ([])
-	const [counter,setCounter] = useState (0)
+  const [inputValue, setInputValue] = useState("");
+  const [task, setTask] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [isHover, setIsHover] = useState(false);
 
-	const handleChande =(e) => {
-		setInputValue(e.target.value)
-	}
-	const handleKeyDown =(e)=>{
-		if (e.keyCode==13) {
-			setTask([...task, inputValue])
-			setInputValue("")
-			setCounter(counter+1)}
-	}
-	const handleRemoveTask =(e)=>{
-			const newtask = task.filter((task)=> task !== e);
-			setTask(newtask)
-			setCounter(counter-1)
+  const handleChande = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      setTask([...task, inputValue]);
+      setInputValue("");
+      setCounter(counter + 1);
+    }
+  };
+  const handleRemoveTask = (e) => {
+    const newtask = task.filter((task) => task !== e);
+    setTask(newtask);
+    setCounter(counter - 1);
+  };
 
-	}
-
-	return (
-		<div className="container">
-			<div>
-				<h1>todos</h1>
-			</div>
-			<ul className="list">
-				<li>
-					<input type='text'
-					placeholder="What needs to be done?" 
-					onChange={handleChande} 
-					onKeyDown={handleKeyDown}
-					value={inputValue}
-					></input>
-				</li>
-				{task.map((t) =>(
-				<li>
-					<span>{t}</span>
-					<i className="fa fa-xmark" onClick={()=>handleRemoveTask(t)} ></i></li>))}
-				<li>{counter} items left</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div>
+        <h1 className="todos">todos</h1>
+      </div>
+      <ul className="list">
+        <li>
+          <input
+            type="text"
+            placeholder="What needs to be done?"
+            onChange={handleChande}
+            onKeyDown={handleKeyDown}
+			value={inputValue}
+          ></input>
+        </li>
+        {task.map((t) => (
+          <li
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+          >
+        	<span>{t}</span>
+            {isHover && (
+                <i
+                  className={`clear fa fa-xmark`}
+                  onClick={() => handleRemoveTask(t)}
+                ></i>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div>{counter} items left</div>
+    </div>
+  );
 };
 
 export default Home;
